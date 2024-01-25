059b670b23d6d439a316a0fcce0bf549a02374bd
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-01-25 12:09:13 +0500

    Add domain models

---
+++ src/CachingIssues/HashCodeIssue.Api/Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿namespace HashCodeIssue.Api.Domain.Entities;
+
+/// <summary>
+/// Represents a storage file.
+/// </summary>
+public class User
+{
+    public Guid Id { get; set; }
+    
+    /// <summary>
+    /// Gets or sets the first name.
+    /// </summary>
+    public string FirstName { get; set; } = default!;
+    
+    /// <summary>
+    /// Gets or sets the last name
+    /// </summary>
+    public string LastName { get; set; } = default!;
+    
+    /// <summary>
+    /// Gets or sets the file name.
+    /// </summary>
+    public string EmailAddress { get; set; } = default!;
+}
